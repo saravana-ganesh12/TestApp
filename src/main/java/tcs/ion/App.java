f89f364e0a81abb64e88e6138c2da5ce5f5da85a
saravana-ganesh12 <63070727+saravana-ganesh12@users.noreply.github.com>
2021-01-03 14:07:04 +0530

    Update App.java

--- src/main/java/tcs/ion/App.java
+++ src/main/java/tcs/ion/App.java
@@ -5,6 +5,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is Testing DevOps pipeline" );
+        System.out.println( "This is my first internship" );
     }
 }
